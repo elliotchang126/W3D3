1082ecb74f896a8f15a6407cf7cd75b7558babe9
Elliot Chang <elliotchang126@gmail.com>
2023-06-07 16:11:38 -0700

    binary search

--- practice-for-ch-ruby-recursion-long-practice-main/solutions.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/solutions.rb
@@ -44,4 +44,33 @@ def fib(n, memo = {})
     memo[n] = fib(n-1, memo).last + fib(n-2, memo).last unless memo.has_key?(n)
     arr << memo[n]
 end
+require "byebug"
+def bsearch(arr, target)
+    return nil if arr.length == 1 && target != arr[0]
 
+    middle_index = arr.length / 2
+    # debugger
+    if arr[middle_index] == target
+        return middle_index
+    elsif target > arr[middle_index]
+        right_index = bsearch(arr[middle_index..-1], target)
+        if right_index
+            right_index + (arr.length / 2)
+        end
+    else
+        bsearch(arr[0...middle_index], target)
+    end
+end
+
+# left = arr[0...middle_index]
+# right = arr[middle_index..-1]
+# debugger
+# if arr[middle_index] == target
+#     return middle_index
+# else
+#     if left_index = bsearch(left, target)
+#         return left_index if left[left_index] == target
+#     else right_index = bsearch(right, target)
+#         return right_index + left.length if right[right_index] == target
+#     end
+# end
