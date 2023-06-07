79701a04b6795273c1533220207aa5f9817c306c
Wizzerrd <>
2023-06-07 11:52:45 -0700

    Add exponents methods

--- practice-for-ch-ruby-recursion-long-practice-main/solutions.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/solutions.rb
@@ -3,3 +3,21 @@ def rec_range(min, max)
     range(min, max - 1) + [max - 1]
 end
 
+def exp_1(b,n)
+    return 1 if n == 0
+    return b if n == 1
+    exp_1(b,n-1) * b
+end
+
+def exp_2(b,n)
+    return 1 if n == 0
+    return b if n == 1
+    if n.even?  
+        exp(b, n / 2) * exp(b, n / 2) 
+    else 
+        b * (exp(b, (n - 1) / 2) ** 2)
+    end
+end
+
+
+
